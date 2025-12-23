434a9cd0ebff5f2766b7d101a1e3081e132e1574
shakchunni <ashfaq601230@gmail.com>
2025-12-23 21:32:28 +0600

    reduced overview height on mobile

--- src/app/(staff)/general-admission/components/PatientTable/components/AdmissionOverview/AdmissionOverview.tsx
+++ src/app/(staff)/general-admission/components/PatientTable/components/AdmissionOverview/AdmissionOverview.tsx
@@ -77,7 +77,7 @@ const AdmissionOverview: React.FC<AdmissionOverviewProps> = ({
     <ModalShell
       isOpen={isOpen}
       onClose={onClose}
-      className="w-full max-w-[90%] md:max-w-[85%] lg:max-w-[80%] h-[95vh] sm:h-[90vh] rounded-3xl overflow-hidden flex flex-col"
+      className="w-full max-w-[90%] md:max-w-[85%] lg:max-w-[80%] h-[80vh] sm:h-[90vh] rounded-3xl overflow-hidden flex flex-col"
     >
       <ModalHeader
         icon={Activity}
